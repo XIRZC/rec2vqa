124e2d388139f633ec4aa64f786be10a97ba6be0
mrxir <zicongxie@foxmail.com>
2022-05-07 14:28:31 +0800

    DEL resttutorial folder, and FIX backend imgupload and vqa create api

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,9 +1,20 @@
 # Create your models here.
 from django.db import models
 
+class IMG(models.Model):
+    img = models.ImageField(upload_to='custom/')
+    def __str__(self):
+        return self.img.name
+    class Meta:
+        db_table = 'img'
+        ordering = ['id']
+        verbose_name = 'IMGTable'
+        verbose_name_plural = verbose_name
+
 class REC(models.Model):
     referring_expression = models.CharField(max_length=255, verbose_name='referring_expression')
-    image = models.ImageField(verbose_name='image')
+    # image = models.ImageField(verbose_name='image', upload_to='custom/')
+    img = models.ForeignKey(IMG, on_delete=models.CASCADE)
     result = models.CharField(max_length=255, verbose_name='result', null=True, blank=True)
     result_image = models.ImageField(verbose_name='result_image', null=True, blank=True)
 
@@ -18,7 +29,8 @@ class REC(models.Model):
 
 class VQA(models.Model):
     question = models.CharField(max_length=255, verbose_name='question')
-    image = models.ImageField(verbose_name='image')
+    img = models.ForeignKey(IMG, on_delete=models.CASCADE)
+    # image = models.ImageField(verbose_name='image', upload_to='custom/')
     answer = models.CharField(max_length=255, verbose_name='answer', null=True, blank=True)
     rec = models.ForeignKey(REC, related_name='vqas', on_delete=models.CASCADE)
     def __str__(self):
@@ -27,12 +39,4 @@ class VQA(models.Model):
         db_table = 'vqa'
         ordering = ['id']
         verbose_name = 'VQATable'
-        verbose_name_plural = verbose_name
-
-class IMG(models.Model):
-    img = models.ImageField(upload_to='custom/')
-    class Meta:
-        db_table = 'img'
-        ordering = ['id']
-        verbose_name = 'IMGTable'
         verbose_name_plural = verbose_name
--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,11 +1,14 @@
+from unittest.mock import seal
 from api import models
 from rest_framework import serializers
 from api.models import REC, VQA, IMG
+import os
+from django.conf import settings
 class VQASerializer(serializers.ModelSerializer):
-    # rec = serializers.ReadOnlyField(source='dependency.referring_expressioin')
+    # rec = serializers.SlugRelatedField(read_only=True, slug_field='referring_expression')
     class Meta:
         model = VQA
-        fields = ('id', 'question', 'image', 'answer')
+        fields = ('id', 'question', 'img', 'answer', 'rec')
 
 class RECSerializer(serializers.ModelSerializer):
     vqas = VQASerializer(many=True, read_only=True)
@@ -13,7 +16,7 @@ class RECSerializer(serializers.ModelSerializer):
     #     many=True, view_name='vqa-detail', read_only=True)
     class Meta:
         model = REC
-        fields = ('id', 'referring_expression', 'image', 'result', 'result_image', 'vqas')
+        fields = ('id', 'referring_expression', 'img', 'result', 'result_image', 'vqas')
 
 # class RECSerializer(serializers.HyperlinkedModelSerializer):
 #     vqas = serializers.HyperlinkedRelatedField(
@@ -26,3 +29,12 @@ class IMGSerializer(serializers.ModelSerializer):
     class Meta:
         model = IMG
         fields = ('id', 'img')
+
+    def create(self, validated_data):
+        filename = 'custom/' + validated_data['img'].name
+        print(filename)
+        try:
+            instance = IMG.objects.get(img=filename)
+            return instance
+        except:
+            return IMG.objects.create(**validated_data)
